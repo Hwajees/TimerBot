fc74f99a020d8a78569e335f9856667826a1e32a
Hwajees <dr.cortex.313@gmail.com>
2025-10-16 16:30:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -164,4 +164,7 @@ async def main():
     )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()  # يسمح بإعادة استخدام حلقة الأحداث الحالية
+    import asyncio
+    asyncio.get_event_loop().run_until_complete(main())
